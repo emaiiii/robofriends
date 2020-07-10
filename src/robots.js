e954a9e5ef708a0cb12e82a7f20b98d491d37d02
emaiiii <airwickmai@gmail.com>
2020-07-10 12:46:31 -0700

    first commit

---
+++ src/robots.js
@@ -0,0 +1,44 @@
+export const robots = [
+{
+	id: 1, 
+	name: 'Erik Mai',
+	username: 'MaiMeatStick',
+	email: 'airwickmai@gmail.com'
+},
+{
+	id: 2, 
+	name: 'Cindy Phan',
+	username: 'CindyAngel10',
+	email: 'cindyangel@gmail.com'
+},
+{
+	id: 3, 
+	name: 'Hieu Le',
+	username: 'hugheslyfe',
+	email: 'hieule17@gmail.com'
+},
+{
+	id: 4, 
+	name: 'Dustin Mai',
+	username: 'dusteez',
+	email: 'dustinmnmn@gmail.com'
+},
+{
+	id: 5, 
+	name: 'Kevin Mai',
+	username: 'crucifix',
+	email: 'kmai09@gmail.com'
+},
+{
+	id: 6, 
+	name: 'Steven Bui',
+	username: 'SimpleDimple',
+	email: 'sbui13@gmail.com'
+},
+{
+	id: 7, 
+	name: 'Dustin Bui',
+	username: 'Pepsi009',
+	email: 'dbui12@gmail.com'
+}
+];
